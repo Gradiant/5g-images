41bca4e343d1c1d5e0bbb7c906ac24d696a3aef0
Carlos Giraldo <cgiraldo@gradiant.org>
2021-02-23 09:37:27 +0100

    Updated docs

--- docs/open5gs-and-srslte/configure_cgnat.sh
+++ docs/open5gs-and-srslte/configure_cgnat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose exec upf iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE;
-#docker-compose exec ue ip route replace default via 10.45.0.1
+docker-compose exec ue ip route replace default via 10.45.0.1
