e17afcd626d7b6e16551fed43093e3a0d6be48d9
Iria <imiguez@gradiant.org>
2021-03-03 10:55:56 +0000

    Added test UERANSIM

--- ueransim/entrypoint.sh
+++ ueransim/entrypoint.sh
@@ -7,24 +7,26 @@ then
         echo "Usage : $0 [gnb|ue]"
         exit
 fi
+if [ -n "${AMF_HOSTNAME}" ] ; then
+    export AMF_ADDR="$(host -4 $AMF_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
+fi
+if [ -n "${GNB_HOSTNAME}" ] ; then
+    export GNB_ADDR="$(host -4 $GNB_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
+fi
 
-export AMF_ADDR="$(host -4 amf |awk '/has.*address/{print $NF; exit}')"
-export GNB_ADDR="$(host -4 gnb |awk '/has.*address/{print $NF; exit}')"
 command=$1
 shift
 
 case "$command" in
 
 ue)  echo "Launching ue"
-    envsubst < /etc/ueransim/open5gs-ue.yaml > open5gs-ue.yaml
-    /root/UERANSIM/build/nr-ue -c open5gs-ue.yaml
+    envsubst < /etc/ueransim/ue.yaml > ue.yaml
+    /root/UERANSIM/build/nr-ue -c ue.yaml
     ;;
 gnb)  echo "Launching gnb"
-    envsubst < /etc/ueransim/open5gs-gnb.yaml > open5gs-gnb.yaml
-    /root/UERANSIM/build/nr-gnb -c open5gs-gnb.yaml
+    envsubst < /etc/ueransim/gnb.yaml > gnb.yaml
+    /root/UERANSIM/build/nr-gnb -c gnb.yaml
     ;;
-*) echo "unknown component $1. should be epc, enb or ue."
+*) echo "unknown component $1. should be gnb or ue."
    ;;
 esac
-
-echo "Hi"
