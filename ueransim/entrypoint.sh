e61361b8a53593c36581dd5bd2e2b4c6e8ceac64
Carlos Giraldo <cgiraldo@gradiant.org>
2021-03-03 15:47:48 +0100

    optimization of ueransim image

--- ueransim/entrypoint.sh
+++ ueransim/entrypoint.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 if [ $# -lt 1 ]
 then
         echo "Usage : $0 [gnb|ue]"
         exit
 fi
-if [ -n "${AMF_HOSTNAME}" ] ; then
-    export AMF_ADDR="$(host -4 $AMF_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
-fi
-if [ -n "${GNB_HOSTNAME}" ] ; then
-    export GNB_ADDR="$(host -4 $GNB_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
-fi
 
 command=$1
-shift
 
 case "$command" in
 
-ue)  echo "Launching ue"
+ue)  echo "Launching ue: nr-ue -c ue.yaml"
+    if [[ ! -z "${GNB_HOSTNAME}" ]] ; then
+        export GNB_ADDR="$(host -4 $GNB_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
+    fi
     envsubst < /etc/ueransim/ue.yaml > ue.yaml
-    /root/UERANSIM/build/nr-ue -c ue.yaml
+    nr-ue -c ue.yaml
     ;;
-gnb)  echo "Launching gnb"
+gnb)  echo "Launching gnb: nr-gnb -c gnb.yaml"
+    if [[ ! -z "${UE_HOSTNAME}" ]] ; then
+        export GNB_ADDR="$(host -4 $GNB_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
+    fi
+    if [[ ! -z "${AMF_HOSTNAME}" ]] ; then
+        export AMF_ADDR="$(host -4 $AMF_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
+    fi
+
     envsubst < /etc/ueransim/gnb.yaml > gnb.yaml
-    /root/UERANSIM/build/nr-gnb -c gnb.yaml
+    nr-gnb -c gnb.yaml
     ;;
-*) echo "unknown component $1. should be gnb or ue."
+*) echo "unknown component $1 is not a component (gnb or ue). Running $@ as command"
+   $@
    ;;
 esac
