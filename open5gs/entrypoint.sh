f18264d222055bd0521fd98291ddf9c93410eb1c
Carlos Giraldo <cgiraldo@gradiant.org>
2021-03-10 09:01:11 +0100

    Automate CG-NAT configuration in upf with ENABLE_NAT env variable

--- open5gs/entrypoint.sh
+++ open5gs/entrypoint.sh
@@ -9,11 +9,19 @@ function tun_create {
         echo "Creating ogstun device"
         ip tuntap add name ogstun mode tun
     fi
-    ip addr del $IPV4_TUN_SUBNET dev ogstun 2> /dev/null || true
-    ip addr add $IPV4_TUN_SUBNET dev ogstun;
-    #ip addr del cafe::1/64 dev ogstun 2> /dev/null || true
-    #ip addr add cafe::1/64 dev ogstun
+
+    ip addr del $IPV4_TUN_ADDR dev ogstun 2> /dev/null || true
+    ip addr add $IPV4_TUN_ADDR dev ogstun;
+
+    sysctl -w net.ipv6.conf.all.disable_ipv6=0;         
+    ip addr del $IPV6_TUN_ADDR dev ogstun 2> /dev/null || true
+    ip addr add $IPV6_TUN_ADDR dev ogstun
+    
     ip link set ogstun up
+    sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward";
+    if [ "$ENABLE_NAT" = true ] ; then
+      iptables -t nat -A POSTROUTING -s $IPV4_TUN_SUBNET ! -o ogstun -j MASQUERADE;
+    fi
 }
  
  COMMAND=$1
