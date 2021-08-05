42d03acdd1d57de32318dc585e194dca97ec4ba6
Carlos Giraldo <cgiraldo@gradiant.org>
2021-08-05 09:27:13 +0200

    refactoring and updating docs

--- docs/open5gs-and-srslte/configure_cgnat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker-compose -f epc.yaml exec upf iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE;
-docker-compose -f srslte.yaml exec ue ip route replace default via 10.45.0.1
--- docs/open5gs-and-srslte/register_subscriber.sh
+++ docs/open5gs-and-srslte/register_subscriber.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-CONTAINER_NAME=open5gs-and-srslte_mongo_1
+MONGO_CONTAINER=open5gs-and-srslte_mongo_1
 
-IMSI_LIST="001010000000001"
-KI=465B5CE8B199B49FAA5F0A2EE238A6BC
-OPC=E8ED289DEBA952E4283B54E88E6183CA
+: 'open5gs-dbctl: Open5GS Database Configuration Tool (0.9.1)
+COMMANDS:
+   add {imsi key opc}: adds a user to the database with default values
+   add {imsi ip key opc}: adds a user to the database with default values and a IPv4 address for the UE
+   addT1 {imsi key opc}: adds a user to the database with 3 differents apns
+   addT1 {imsi ip key opc}: adds a user to the database with 3 differents apns and the same IPv4 address for the each apn
+   remove {imsi}: removes a user from the database
+   reset: WIPES OUT the database and restores it to an empty default
+   static_ip {imsi ip4}: adds a static IP assignment to an already-existing user
+   static_ip6 {imsi ip6}: adds a static IPv6 assignment to an already-existing user
+   type {imsi type}: changes the PDN-Type of the first PDN: 0 = IPv4, 1 = IPv6, 2 = IPv4v6, 3 = v4 OR v6
+   help: displays this message and exits
+   default values are as follows: APN "internet", dl_bw/ul_bw 1 Gbps, PGW address is 127.0.0.3, IPv4 only
+'
 
-for IMSI in $IMSI_LIST; do  
-  docker exec $CONTAINER_NAME mongo --eval "db.subscribers.update({\"imsi\" : \"$IMSI\"}, { \$setOnInsert: {\"imsi\" : \"$IMSI\",\"subscribed_rau_tau_timer\": NumberInt(12),\"network_access_mode\": NumberInt(2),\"subscriber_status\": NumberInt(0),\"access_restriction_data\": NumberInt(32),\"slice\":[{\"sst\": NumberInt(1),\"default_indicator\":true,\"session\":[{\"name\":\"internet\",\"type\":NumberInt(3),\"pcc_rule\":[],\"ambr\":{\"uplink\":{\"value\":NumberInt(1),\"unit\":NumberInt(3)},\"downlink\":{\"value\":NumberInt(1),\"unit\":NumberInt(3)}},\"qos\":{\"index\":NumberInt(9),\"arp\":{\"priority_level\":NumberInt(8),\"pre_emption_capability\":NumberInt(1),\"pre_emption_vulnerability\":NumberInt(1)}}}]}],\"ambr\":{\"uplink\":{\"value\":NumberInt(1),\"unit\":NumberInt(3)},\"downlink\":{\"value\":NumberInt(1),\"unit\":NumberInt(3)}},\"security\":{\"k\":\"$KI\",\"amf\":\"8000\",\"op\":null,\"opc\":\"$OPC\"}}}, upsert=true);" open5gs
-done
+docker cp open5gs-dbctl $MONGO_CONTAINER:/
+
+docker exec -ti $MONGO_CONTAINER /open5gs-dbctl add 901700000000001 465B5CE8B199B49FAA5F0A2EE238A6BC E8ED289DEBA952E4283B54E88E6183CA
+
