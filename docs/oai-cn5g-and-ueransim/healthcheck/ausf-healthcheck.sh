bc2e88c30341fcb336b65bd240ee1d563c938a2e
Alvaro Vazquez Rodriguez <avrodriguez@gradiant.org>
2021-11-11 15:35:08 +0100

    oai-cn5g docs

---
+++ docs/oai-cn5g-and-ueransim/healthcheck/ausf-healthcheck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eo pipefail
+
+STATUS=0
+AUSF_IP_SBI_INTERFACE=$(ifconfig $SBI_IF_NAME | grep inet | awk {'print $2'})
+AUSF_SBI_PORT_STATUS=$(netstat -tnpl | grep -o "$AUSF_IP_SBI_INTERFACE:$SBI_PORT")
+#Check if entrypoint properly configured the conf file and no parameter is unset(optional)
+NB_UNREPLACED_AT=`cat /openair-ausf/etc/*.conf | grep -v contact@openairinterface.org | grep -c @ || true`
+
+if [ $NB_UNREPLACED_AT -ne 0 ]; then
+        STATUS=1
+        echo "Healthcheck error: UNHEALTHY configuration file is not configured properly"
+fi
+
+if [[ -z $AUSF_SBI_PORT_STATUS ]]; then
+        STATUS=1
+        echo "Healthcheck error: UNHEALTHY SBI TCP/HTTP port $SBI_PORT is not listening."
+fi
+
+exit $STATUS
