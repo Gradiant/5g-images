5ce9ce1b250ae35832e878ea477bfb377a74f8a3
Alvaro Vazquez Rodriguez <avrodriguez@gradiant.org>
2022-02-03 11:23:44 +0100

    open5gs updated v2.4.4

--- open5gs/.cicd/release.sh
+++ open5gs/.cicd/release.sh
@@ -7,7 +7,7 @@ source variables.env
 docker login $REGISTRY -u $REGISTRY_USER -p $REGISTRY_PASSWORD
 if [ -z ${PLATFORM} ]; then
     docker tag $IMAGE:$VERSION $REGISTRY/$IMAGE:$VERSION
-    docker push $IMAGE:$VERSION $REGISTRY/$IMAGE:$VERSION
+    docker push $REGISTRY/$IMAGE:$VERSION
 else
     docker buildx build --push --platform $PLATFORM --pull --tag $REGISTRY/$IMAGE:$VERSION --build-arg version=$VERSION ..
 fi
