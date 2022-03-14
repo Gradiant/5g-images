2666f7e6b48d242b6061af54a1904180f4bda1e6
Carlos Giraldo <cgiraldo@gradiant.org>
2022-03-14 12:06:30 +0100

    fix oai get latest tag

--- images/oai/update_current_tag.sh
+++ images/oai/update_current_tag.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-current_tag=`wget -O- -q https://gitlab.eurecom.fr/api/v4/projects/223/repository/tags | jq -r '.[].name' | tail -1`
+current_tag=`wget -O- -q https://gitlab.eurecom.fr/api/v4/projects/223/repository/tags | jq -r '.[].name' | head -1`
 sed -i "s/TAG=.*/TAG=${current_tag}/g" Makefile
