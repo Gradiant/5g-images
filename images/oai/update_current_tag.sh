0c2d7cf38580487bf81d43eefc1b1264ff176287
Adrián Estévez Barreiro <aebarreiro@gradiant.org>
2022-05-11 18:01:19 +0200

    improved update_current_tag.sh

--- images/oai/update_current_tag.sh
+++ images/oai/update_current_tag.sh
@@ -1,5 +1,25 @@
 #/bin/bash
 
-current_tag=`wget -O- -q https://gitlab.eurecom.fr/api/v4/projects/223/repository/tags | jq -r '.[].name' | head -1`
-echo "updating IMAGE_TAG to ${current_tag} in image_info.sh"
-sed -i "s/IMAGE_TAG=.*/IMAGE_TAG=${current_tag}/g" image_info.sh
+REMOTE_REPO_URL="https://gitlab.eurecom.fr/api/v4/projects/223/repository/tags"
+
+# Fetch the last version available on the remote repository
+CURRENT_TAG=$( curl -sSL ${REMOTE_REPO_URL} | sed -n '/v[0-9.]\+/h; $g; s/^.*\/v\([0-9.]\+\).*$/\1/; $p' )
+# sed explanation
+# -n dont print anything
+# /v[0-9.]\+/h; every match with "v[0-9.]\+" gets stored on hold
+# $g; replace contents of pattern space with hold space, adds last match as last line to buffer
+# s//\1/; replace pattern, removes everything around version
+# $p only last line is printed
+
+# Load IMAGE_TAG
+[[ -f image_info.sh ]] && source image_info.sh || exit 1
+
+# Compare CURRENT_TAG with saved IMAGE_TAG
+[[ ${CURRENT_TAG} == ${IMAGE_TAG} ]] || {
+	echo "Updating old TAG ${IMAGE_TAG} with new TAG ${current_tag}"
+	sed -i "/IMAGE_TAG/s/=[.0-9]\+$/=${CURRENT_TAG}/" image_info.sh
+	exit 0
+} 
+
+# Fail otherwise
+exit 1
