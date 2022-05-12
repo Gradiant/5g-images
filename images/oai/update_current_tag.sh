5fe5d1a2a66ab87d9305cb65e666ae0e358a626c
Adrián Estévez Barreiro <aebarreiro@gradiant.org>
2022-05-12 12:27:27 +0200

    added gitlab support to update script

--- images/oai/update_current_tag.sh
+++ images/oai/update_current_tag.sh
@@ -2,14 +2,24 @@
 
 REMOTE_REPO_URL="https://gitlab.eurecom.fr/api/v4/projects/223/repository/tags"
 
+GH="https://api.github.*"
+GL="https://gitlab.*"
+
 # Fetch the last version available on the remote repository
-CURRENT_TAG=$( curl -sSL ${REMOTE_REPO_URL} | sed -n '/v[0-9.]\+/h; $g; s/^.*\/v\([0-9.]\+\).*$/\1/; $p' )
+if [[ ${REMOTE_REPO_URL} =~ ${GH} ]] ; then
+	CURRENT_TAG=$( curl -sSL ${REMOTE_REPO_URL} | sed -n '/v[0-9.]\+/h; $g; s/^.*\/v\([0-9.]\+\).*$/\1/; $p' )
 # sed explanation
 # -n dont print anything
 # /v[0-9.]\+/h; every match with "v[0-9.]\+" gets stored on hold
 # $g; replace contents of pattern space with hold space, adds last match as last line to buffer
 # s//\1/; replace pattern, removes everything around version
 # $p only last line is printed
+elif [[ ${REMOTE_REPO_URL} =~ ${GL} ]] ; then
+	CURRENT_TAG=$( curl -sSL ${REMOTE_REPO_URL} | jq -r '.[].name' | sed -n '1p' )
+else
+	echo "Error Reading URL"
+	exit 1
+fi
 
 # Load IMAGE_TAG
 [[ -f image_info.sh ]] && source image_info.sh || exit 1
