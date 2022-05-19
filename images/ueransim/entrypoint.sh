05342502331479713f0e55dc7f5fa59c85798221
Carlos Giraldo <cgiraldo@gradiant.org>
2022-05-19 12:15:59 +0200

    Added curl and iperf3 (#89)

--- images/ueransim/entrypoint.sh
+++ images/ueransim/entrypoint.sh
@@ -13,22 +13,26 @@ shift
 
 case "$command" in
 
-ue)  echo "Launching ue: nr-ue -c ue.yaml"
-    if [[ ! -z "${GNB_HOSTNAME}" ]] ; then
-        export GNB_ADDR="$(host -4 $GNB_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
-    fi
+ue) 
+    GNB_IP=${GNB_IP:-"$(host -4 $GNB_HOSTNAME |awk '/has.*address/{print $NF; exit}')"}
+    export GNB_IP
+    echo "GNB_IP: $GNB_IP"
     envsubst < /etc/ueransim/ue.yaml > ue.yaml
+    echo "Launching ue: nr-ue -c ue.yaml"
     nr-ue -c ue.yaml $@
     ;;
-gnb)  echo "Launching gnb: nr-gnb -c gnb.yaml"
-    if [[ ! -z "${GNB_HOSTNAME}" ]] ; then
-        export GNB_ADDR="$(host -4 $GNB_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
-    fi
-    if [[ ! -z "${AMF_HOSTNAME}" ]] ; then
-        export AMF_ADDR="$(host -4 $AMF_HOSTNAME |awk '/has.*address/{print $NF; exit}')"
-    fi
-
+gnb)
+    N2_BIND_IP=${N2_BIND_IP:-"$(ip addr show ${N2_IFACE}  | grep -o 'inet [[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}'| cut -c 6-)"}
+    N3_BIND_IP=${N3_BIND_IP:-"$(ip addr show ${N3_IFACE} | grep -o 'inet [[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}'| cut -c 6-)"}
+    RADIO_BIND_IP=${RADIO_BIND_IP:-"$(ip addr show ${RADIO_IFACE} | grep -o 'inet [[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}'| cut -c 6-)"}
+    AMF_IP=${AMF_IP:-"$(host -4 $AMF_HOSTNAME |awk '/has.*address/{print $NF; exit}')"}
+    export N2_BIND_IP N3_BIND_IP RADIO_BIND_IP AMF_IP
+    echo "N2_BIND_IP: $N2_BIND_IP"
+    echo "N3_BIND_IP: $N3_BIND_IP"
+    echo "RADIO_BIND_IP: $RADIO_BIND_IP"
+    echo "AMF_IP: $AMF_IP"
     envsubst < /etc/ueransim/gnb.yaml > gnb.yaml
+    echo "Launching gnb: nr-gnb -c gnb.yaml"
     nr-gnb -c gnb.yaml $@
     ;;
 *) echo "unknown component $1 is not a component (gnb or ue). Running $@ as command"
