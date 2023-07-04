d9ccb7427dc78d68f11ae80e3977efa5e1ce18f2
Álvaro Vázquez <avrodriguez@gradiant.org>
2023-07-04 08:48:40 +0200

    adds srsran-5g image based on srsRAN-Project (#197)

---
+++ images/srsran-5g/image_info.sh
@@ -0,0 +1,3 @@
+IMAGE_TAG=23_5
+#comma separated list of platforms. If empty, image will not be multiarch.
+PLATFORMS=
---
+++ images/srsran-5g/update_current_tag.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+
+REMOTE_REPO_URL="https://api.github.com/repos/srsran/srsRAN_Project/git/refs/tags/"
+
+GH="https://api.github.*"
+GL="https://gitlab.*"
+
+# Fetch the last version available on the remote repository
+if [[ ${REMOTE_REPO_URL} =~ ${GH} ]] ; then
+	CURRENT_TAG=$( curl -sSL ${REMOTE_REPO_URL} | jq -r '.[].ref' | grep release | cut -d '/' -f3 | cut -c9- | tail -1 )
+# sed explanation
+# -n dont print anything
+# /v[0-9.]\+/h; every match with "v[0-9.]\+" gets stored on hold
+# $g; replace contents of pattern space with hold space, adds last match as last line to buffer
+# s//\1/; replace pattern, removes everything around version
+# $p only last line is printed
+elif [[ ${REMOTE_REPO_URL} =~ ${GL} ]] ; then
+	CURRENT_TAG=$( curl -sSL ${REMOTE_REPO_URL} | jq -r '.[].name' | sed -n '1p' )
+else
+	echo "Error Reading URL"
+	exit 1
+fi
+
+# Load IMAGE_TAG
+[[ -f image_info.sh ]] && source image_info.sh || exit 1
+
+
+# Compare CURRENT_TAG with saved IMAGE_TAG
+[[ ${CURRENT_TAG} == ${IMAGE_TAG} ]] || {
+	echo "Updating old TAG ${IMAGE_TAG} with new TAG ${CURRENT_TAG}"
+	sed -i "/IMAGE_TAG/s/=.*$/=${CURRENT_TAG}/" image_info.sh
+	exit 0
+} 
+
+# Fail otherwise
+exit 1
